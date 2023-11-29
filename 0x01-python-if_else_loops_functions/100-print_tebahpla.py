77606f6fc767b7aeb33ff5a27f8ee1716bd5d4f7
agutuomondi <mwalimuagutu@gmail.com>
2023-11-28 18:16:53 -0800

    100-print_tebahpla.py

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
-for i in range(ord('z'), ord('a') - 1, -2):
+for i in range(122, 96, -1):
+    if i % 2 != 0:
+        i = i - 32
+
     print("{}{}".format(chr(i)), char(i - 33)), end='')
 
