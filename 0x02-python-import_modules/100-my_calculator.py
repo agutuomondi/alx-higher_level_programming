0ef4edad85cb48d89f9541dcb40553be81389925
agutuomondi <mwalimuagutu@gmail.com>
2023-11-30 11:14:34 -0800

    100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,6 +1,24 @@
-#!/usr/bin/python3
-for i in range(122, 96, -1):
-    if i % 2 != 0:
-        i = i - 32
-    print("{}".format(chr(i)), end="")
+import sys
+from calculator_1 import add, sub, mul, div
 
+if len(sys.argv) != 4:
+    print("Usage: {} <a> <operator> <b>".format(sys.argv[0]))
+    sys.exit(1)
+
+a = int(sys.argv[1])
+operator = sys.argv[2]
+b = int(sys.argv[3])
+
+if operator == '+':
+    result = add(a, b)
+elif operator == '-':
+    result = sub(a, b)
+elif operator == '*':
+    result = mul(a, b)
+elif operator == '/':
+    result = div(a, b)
+else:
+    print("Unknown operator. Available operators: +, -, * and /")
+    sys.exit(1)
+
+print("{} {} {} = {}".format(a, operator, b, result))
