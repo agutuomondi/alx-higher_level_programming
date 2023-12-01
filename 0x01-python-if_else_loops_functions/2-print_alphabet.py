8853668db3d1ee2017e876bec2fdc421eb614bc4
agutuomondi <mwalimuagutu@gmail.com>
2023-12-01 06:50:26 -0800

    2-print_alphabet.py

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-for i in range(97, 123):
-    print("{}".format(char(i)), end='')
+if __name__ == "__main__":
+    for i in range(ord('a'), ord('z') + 1):
+        print("{:c}".format(i), end="")
 
