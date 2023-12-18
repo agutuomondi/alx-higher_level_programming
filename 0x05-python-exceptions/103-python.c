7ae41dd2a3999cde82a0f9d2e289ce233dd953f3
agutuomondi <mwalimuagutu@gmail.com>
2023-12-18 02:01:20 -0800

    103-python.c

--- 0x05-python-exceptions/103-python.c
+++ 0x05-python-exceptions/103-python.c
@@ -1,64 +1,79 @@
 #include <Python.h>
 
-void print_list_info(PyListObject *list);
-void print_bytes_info(PyBytesObject *bytes);
-void print_float_info(PyFloatObject *flt);
+void print_python_list(PyObject *p);
+void print_python_bytes(PyObject *p);
+void print_python_float(PyObject *p);
 
-void print_python_object_info(PyObject *p) {
-    if (p == NULL) {
-        fprintf(stderr, "[ERROR] Invalid Object\n");
+void print_python_list(PyObject *p) {
+    Py_ssize_t size, i;
+    const char *type;
+    PyListObject *list = (PyListObject *)p;
+
+    if (!list || !PyList_Check(list)) {
+        fprintf(stderr, "[ERROR] Invalid List Object\n");
         return;
     }
 
-    if (PyList_Check(p)) {
-        print_list_info((PyListObject *)p);
-    } else if (PyBytes_Check(p)) {
-        print_bytes_info((PyBytesObject *)p);
-    } else if (PyFloat_Check(p)) {
-        print_float_info((PyFloatObject *)p);
-    } else {
-        fprintf(stderr, "[ERROR] Unknown Object Type\n");
-    }
-}
-
-void print_list_info(PyListObject *list) {
-    Py_ssize_t size = PyList_Size((PyObject *)list);
-    Py_ssize_t alloc = list->allocated;
+    size = PyList_Size(p);
 
     printf("[*] Python list info\n");
     printf("[*] Size of the Python List = %ld\n", size);
-    printf("[*] Allocated = %ld\n", alloc);
 
-    for (Py_ssize_t i = 0; i < size; i++) {
-        PyObject *item = PyList_GetItem((PyObject *)list, i);
-        printf("Element %ld: %s\n", i, Py_TYPE(item)->tp_name);
-        print_python_object_info(item);
+    for (i = 0; i < size; i++) {
+        type = Py_TYPE(PyList_GetItem(p, i))->tp_name;
+        printf("Element %ld: %s\n", i, type);
+
+        if (strcmp(type, "bytes") == 0)
+            print_python_bytes(PyList_GetItem(p, i));
+        else if (strcmp(type, "float") == 0)
+            print_python_float(PyList_GetItem(p, i));
     }
 }
 
-void print_bytes_info(PyBytesObject *bytes) {
-    Py_ssize_t size = PyBytes_Size((PyObject *)bytes);
+void print_python_bytes(PyObject *p) {
+    Py_ssize_t size, i;
+    PyBytesObject *bytes = (PyBytesObject *)p;
+
+    if (!bytes || !PyBytes_Check(bytes)) {
+        fprintf(stderr, "[ERROR] Invalid Bytes Object\n");
+        return;
+    }
 
     printf("[.] bytes object info\n");
+    size = PyBytes_Size(p);
     printf("  size: %ld\n", size);
-    
-    if (size > 0) {
-        printf("  trying string: %s\n", bytes->ob_sval);
-        printf("  first %ld bytes: ", (size > 10) ? 10 : size);
-        for (Py_ssize_t i = 0; i < size && i < 10; i++) {
-            printf("%02hhx ", bytes->ob_sval[i]);
-        }
-        printf("\n");
-    }
+
+    printf("  first %ld bytes: ", (size > 10) ? 10 : size);
+    for (i = 0; i < size && i < 10; i++)
+        printf("%02hhx ", bytes->ob_sval[i]);
+    printf("\n");
 }
 
-void print_float_info(PyFloatObject *flt) {
+void print_python_float(PyObject *p) {
+    char *buffer = NULL;
+
+    if (!p || !PyFloat_Check(p)) {
+        fprintf(stderr, "[ERROR] Invalid Float Object\n");
+        return;
+    }
+
     printf("[.] float object info\n");
-    printf("  value: %f\n", flt->ob_fval);
+
+    buffer = PyOS_double_to_string(((PyFloatObject *)p)->ob_fval, 'r', 0,
+                                    Py_DTSF_ADD_DOT_0, NULL);
+    
+    if (buffer) {
+        printf("  value: %s\n", buffer);
+        PyMem_Free(buffer);
+    } else {
+        fprintf(stderr, "[ERROR] Failed to convert float to string\n");
+    }
 }
 
 int main() {
     // Example usage
+    Py_Initialize();
+
     PyObject *list = PyList_New(3);
     PyObject *bytes = PyBytes_FromString("Hello, World!");
     PyObject *flt = PyFloat_FromDouble(3.14);
@@ -67,13 +82,13 @@ int main() {
     PyList_SetItem(list, 1, bytes);
     PyList_SetItem(list, 2, flt);
 
-    print_python_object_info(list);
-    print_python_object_info(bytes);
-    print_python_object_info(flt);
+    print_python_list(list);
 
     Py_DECREF(list);
     Py_DECREF(bytes);
     Py_DECREF(flt);
+
+    Py_Finalize();
 
     return 0;
 }
