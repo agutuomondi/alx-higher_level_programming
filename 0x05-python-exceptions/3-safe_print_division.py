9b0063af5c87e4fa99c31dca4afa1aa564983fb7
agutuomondi <mwalimuagutu@gmail.com>
2023-12-18 01:44:37 -0800

    3-safe_print_division.py

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -3,8 +3,13 @@
 def safe_print_division(a, b):
     try:
         div = a / b
-    except (TypeError, ZeroDivisionError):
+    except ZeroDivisionError:
         div = None
-    else:
-        print("Inside result: {}".format(div))
+        print("Error: Division by zero")
+    except TypeError:
+        div = None
+        print("Error: Invalid types for division")
     return div
+
+result = safe_print_division(4, 2)
+print("Inside result:", result)
