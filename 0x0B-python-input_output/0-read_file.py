89cba3328844e326533982f946bcbf1d91c4fec0
agutuomondi <mwalimuagutu@gmail.com>
2024-01-10 10:06:59 -0800

    0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
 def read_file(filename=""):
-    try:
-        with open(filename, encoding="utf-8") as f:
-            print(f.read(), end="")
-    except FileNotFoundError:
-        print(f"Error: File '{filename}' not found.")
-    except Exception as e:
-        print(f"An error occurred: {e}")
+    with open(filename, encoding="utf-8") as f:
+        print(f.read(), end="")
