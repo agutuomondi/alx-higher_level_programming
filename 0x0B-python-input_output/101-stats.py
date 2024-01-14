66fce4d82b7f6b58a1f38753e8ca728686aa0700
agutuomondi <mwalimuagutu@gmail.com>
2024-01-14 11:32:34 -0800

    101-stats.py

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,38 +1,47 @@
 #!/usr/bin/python3
+
+
+
 def print_stats(size, status_codes):
-    print(f"File size: {size}")
-    for key, value in sorted(status_codes.items()):
-        print(f"{key}: {value}")
+
+    print("File size: {}".format(size))
+    for key in sorted(status_codes):
+        print("{}: {}".format(key, status_codes[key]))
+
 
 if __name__ == "__main__":
     import sys
 
     size = 0
     status_codes = {}
-    valid_codes = {'200', '301', '400', '401', '403', '404', '405', '500'}
+    valid_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
     count = 0
 
     try:
         for line in sys.stdin:
-            count = count + 1 if count < 10 else 1
+            if count == 10:
+                print_stats(size, status_codes)
+                count = 1
+            else:
+                count += 1
+
+            line = line.split()
 
             try:
-                size += int(line.split()[-1])
+                size += int(line[-1])
             except (IndexError, ValueError):
                 pass
 
             try:
-                code = line.split()[-2]
-                if code in valid_codes:
-                    status_codes[code] = status_codes.get(code, 0) + 1
+                if line[-2] in valid_codes:
+                    if status_codes.get(line[-2], -1) == -1:
+                        status_codes[line[-2]] = 1
+                    else:
+                        status_codes[line[-2]] += 1
             except IndexError:
                 pass
 
-            if count == 10:
-                print_stats(size, status_codes)
-
-        if count != 0:
-            print_stats(size, status_codes)
+        print_stats(size, status_codes)
 
     except KeyboardInterrupt:
         print_stats(size, status_codes)
