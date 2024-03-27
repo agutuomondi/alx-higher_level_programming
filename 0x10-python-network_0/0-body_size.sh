58a39cf848aa01c56393527b60cfd5f880a57b2f
agutuomondi <mwalimuagutu@gmail.com>
2024-03-27 14:35:02 -0700

    0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
-curl -s "$1" | wc -c
+
+if [ -z "$1" ]; then
+    echo "Usage: $0 <URL>"
+    exit 1
+fi
+
+size=$(curl -sI "$1" | awk '/Content-Length/ {print $2}' | tr -d '\r')
+
+if [ -z "$size" ]; then
+    size=$(curl -s "$1" | wc -c)
+fi
+
+echo "$size"
