e06073320cc18c4016645354a327b944769fc51d
agutuomondi <mwalimuagutu@gmail.com>
2024-04-01 12:11:56 -0700

    100-github_commits.py

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,28 +1,29 @@
 #!/usr/bin/python3
 
-""" Python script that takes in a URL and an email address,
-sends a POST request to the passed URL with the email as a parameter,
-and finally displays the body of the response.
+""" Python script takes URL and email address,
+sends a POST request passed URL with email as parameter,
+and displays body of response.
 Usage: ./6-post_email.py <URL> <email>
   - Displays the body of the response.
 """
-#!/usr/bin/python3
 
 import requests
 import sys
 
 def get_latest_commits(owner_name, repo_name):
     url = f"https://api.github.com/repos/{owner_name}/{repo_name}/commits"
-    try:
-        response = requests.get(url)
-        response.raise_for_status()  # Raise an exception for HTTP errors
+    response = requests.get(url)
+    return response
+
+def display_commits(response):
+    if response.status_code == 200:
         commits = response.json()[:10]  # Get the latest 10 commits
         for commit in commits:
             sha = commit['sha']
             author_name = commit['commit']['author']['name']
             print(f"{sha}: {author_name}")
-    except requests.exceptions.RequestException as e:
-        print(f"Error: Failed to retrieve commits - {e}")
+    else:
+        print("Error: Failed to retrieve commits from the repository.")
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
@@ -32,5 +33,5 @@ if __name__ == "__main__":
     owner_name = sys.argv[1]
     repo_name = sys.argv[2]
 
-    get_latest_commits(owner_name, repo_name)
-
+    response = get_latest_commits(owner_name, repo_name)
+    display_commits(response)
