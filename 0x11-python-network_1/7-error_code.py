de3d815716b99a87aa13268f1c5b1d5e1f6f8c51
agutuomondi <mwalimuagutu@gmail.com>
2024-04-01 13:17:22 -0700

    7-error_code.py

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 
-import requests
 import sys
 
-if __name__ == "__main__":
-    url = sys.argv[1]
+import requests
 
-    response = requests.get(url)
-    if response.status_code >= 400:
-        print("Error code:", response.status_code)
+if __name__ == "__main__":
+    r = requests.get(sys.argv[1])
+    if r.status_code >= 400:
+        print("Error code: {}".format(r.status_code))
     else:
-        print(response.text)
+        print(r.text)
