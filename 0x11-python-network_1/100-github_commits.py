48d4f4aed913edd3697ad59ba4c11846d42e4f27
agutuomondi <mwalimuagutu@gmail.com>
2024-04-01 13:08:29 -0700

    100-github_commits.py

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -10,28 +10,17 @@ Usage: ./6-post_email.py <URL> <email>
 import requests
 import sys
 
-def get_latest_commits(owner_name, repo_name):
-    url = f"https://api.github.com/repos/{owner_name}/{repo_name}/commits"
-    response = requests.get(url)
-    return response
-
-def display_commits(response):
-    if response.status_code == 200:
-        commits = response.json()[:10]  # Get the latest 10 commits
-        for commit in commits:
-            sha = commit['sha']
-            author_name = commit['commit']['author']['name']
-            print(f"{sha}: {author_name}")
-    else:
-        print("Error: Failed to retrieve commits from the repository.")
 
 if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        print("Usage: ./script.py <owner_name> <repo_name>")
-        sys.exit(1)
+    url = "https://api.github.com/repos/{}/{}/commits".format(
+        sys.argv[2], sys.argv[1])
 
-    owner_name = sys.argv[1]
-    repo_name = sys.argv[2]
-
-    response = get_latest_commits(owner_name, repo_name)
-    display_commits(response)
+    r = requests.get(url)
+    commits = r.json()
+    try:
+        for i in range(10):
+            print("{}: {}".format(
+                commits[i].get("sha"),
+                commits[i].get("commit").get("author").get("name")))
+    except IndexError:
+        pass
