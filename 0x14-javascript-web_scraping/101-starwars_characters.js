abfcc946fc56b10e8917ba69e4872082a38f0a18
agutuomondi <mwalimuagutu@gmail.com>
2024-04-24 15:04:24 -0700

    101-starwars_characters.js

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,39 +1,32 @@
 #!/usr/bin/node
 
-const axios = require('axios');
-
-// Construct the SWAPI URL based on the film ID from the command line arguments
-const apiUrl = `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`;
-
-// Make a request to the SWAPI URL
-axios.get(apiUrl)
-  .then(response => {
-    // Parse the JSON response body to get the list of characters
-    const characters = response.data.characters;
-
-    // Call the function to print characters, starting from index 0
-    printCharacters(characters, 0);
-  })
-  .catch(error => {
-    console.error('Error:', error);
-  });
+const request = require('request');
+const apiUrl = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
 
 // Function to recursively print characters
-async function printCharacters(characters, index) {
-  try {
-    // Make a request to fetch information about the character at the given index
-    const response = await axios.get(characters[index]);
-    
-    // Print the name of the character
-    console.log(response.data.name);
+function printCharacters (characters, index) {
+  request(characters[index], function (error, response, body) {
+    // Check for errors in the request
+    if (!error) {
+      // Print the name of the character
+      console.log(JSON.parse(body).name);
 
-    // Check if there are more characters to print
-    if (index + 1 < characters.length) {
-      // Recursively call the function for the next character
-      await printCharacters(characters, index + 1);
+      // Check if there are more characters to print
+      if (index + 1 < characters.length) {
+        // Recursively call the function for the next character
+        printCharacters(characters, index + 1);
+      }
     }
-  } catch (error) {
-    console.error('Error:', error);
-  }
+  });
 }
+
+// Make a request to the SWAPI URL
+request(apiUrl, function (error, response, body) {
+  // Check for errors in the request
+  if (!error) {
+    const characters = JSON.parse(body).characters;
+
+    printCharacters(characters, 0);
+  }
+});
 
